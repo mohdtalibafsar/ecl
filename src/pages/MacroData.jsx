d6c1a92db04a04e9205afbfda3ba67d2278c07d1
mohdtalibGT <Mohd.Talib@in.gt.com>
2023-11-27 22:42:45 +0530

    added features

--- src/pages/MacroData.jsx
+++ src/pages/MacroData.jsx
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
-
+import React from "react";
+import classes from "./MacroData.module.css";
 const MacroData = () => {
-  return <div className="title"> MacroData</div>;
-};
+  return (
+
+<div className={classes.MacroData_page}>
+      <div className={classes.navbar2}>
+        <Button text="Economic Indicators" />
+        <Button text="Financial Metric" />
+        <Button text="Portfolio Distribution" />
+      </div>
+      </div>
+
+  );
+  };
+
+  const Button = ({ text }) => {
+    return <button className={classes.btn2}>{text}</button>;
+  };
 
 export default MacroData;
--- src/pages/Pd.jsx
+++ src/pages/Pd.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import classes from "./pd.module.css"
 const Pd = () => {
-  return <div className="title"> PD</div>;
-};
+  return (
+
+<div className={classes.pd_page}>
+      <div className={classes.navbar3}>
+        <Button text="Recalculate PD " />
+        <Button text="LGD" />
+        <Button text="EAD" />
+      </div>
+      </div>
+
+  );
+  };
+
+  const Button = ({ text }) => {
+    return <button className={classes.btn3}>{text}</button>;
+  };
 
 export default Pd;
